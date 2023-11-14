5fa471cfb615e64f5968661a64fecf1f59d5797b
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-14 11:43:49 +0900

    HelloControllerを作成

---
+++ src/main/java/com/example/SpringBootRESTAPI/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.SpringBootRESTAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping(value="/")
+	public String index() {
+		return "アクセス成功です";
+	}
+	
+	@GetMapping(value="/hello")
+	public String hello() {
+		return "Hello World";
+	}
+	
+	@GetMapping(value="/welcome")
+	public String welcome() {
+		return "Spring Bootへようこそ";
+	}
+	
+}
