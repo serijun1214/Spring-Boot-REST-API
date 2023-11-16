6910bb34c32439dc2df312395a3fd47108d24193
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-16 09:48:51 +0900

    キャッシュの設定を追加

--- src/main/java/com/example/SpringBootRESTAPI/SpringBootRestApiApplication.java
+++ src/main/java/com/example/SpringBootRESTAPI/SpringBootRestApiApplication.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import com.example.SpringBootRESTAPI.model.Item;
 import com.example.SpringBootRESTAPI.repo.ItemRepository;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringBootRestApiApplication implements CommandLineRunner {
 	
 	@Autowired
--- src/main/java/com/example/SpringBootRESTAPI/service/ItemService.java
+++ src/main/java/com/example/SpringBootRESTAPI/service/ItemService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import com.example.SpringBootRESTAPI.model.Item;
@@ -16,6 +19,7 @@ public class ItemService {
 	@Autowired
 	private ItemRepository itemRepository;
 	
+	@Cacheable("getItems")
 	public List<Item> getAllItems() {
 		List<Item> allItems = new ArrayList<>();
 		itemRepository.findAll().forEach(allItems::add);
@@ -23,20 +27,30 @@ public class ItemService {
 		return allItems;
 	}
 	
+	@Cacheable(value="getItem", key="#p0")
 	public Optional<Item> getItem(Long itemId) {
 		return itemRepository.findById(itemId);
 	}
 	
+	@CacheEvict(value="getItem", allEntries=true)
 	public void addItem (Item item) {
 		itemRepository.save(item);
 	}
 
+	@Caching(evict= {
+			@CacheEvict(value="getItem", key="#p0"),
+			@CacheEvict(value="getItem", allEntries=true)
+	})
 	public void updateItem(Long itemId, Item item) {
 		if(itemRepository.findById(itemId).get() != null) {
 			itemRepository.save(item);
 		}
 	}
 
+	@Caching(evict= {
+			@CacheEvict(value="getItem", key="#p0"),
+			@CacheEvict(value="getItem", allEntries=true)
+	})
 	public void deleteItem(Long itemId) {
 		itemRepository.deleteById(itemId);	
 	}
